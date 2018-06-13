c941e09703dd0f89ccfcc5e08391a568db09289c
Thomas Galliker <thomas@bluewin.ch>
2018-06-13 17:03:17 +0200

    Release 1.0.0-pre2

--- Paging.Forms/Paging.Forms/Properties/AssemblyInfo.cs
+++ Paging.Forms/Paging.Forms/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Paging")]
+[assembly: AssemblyProduct("Paging.Forms")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,4 +24,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0-pre1")]
+[assembly: AssemblyFileVersion("1.0.0-pre2")]
