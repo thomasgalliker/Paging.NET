28c4b289dfde5097bb847ea8aca5246e0e362f80
Thomas Galliker <thomas@bluewin.ch>
2019-01-25 10:26:35 +0100

    Add more comments

--- Paging.Shared/PaginationSet.cs
+++ Paging.Shared/PaginationSet.cs
@@ -37,29 +37,38 @@ namespace Paging
 
         /// <summary>
         /// The page index of the currently filtered set.
+        /// CurrentPage is 1-indexed, means the first page is CurrentPage = 1 (not CurrentPage = 0).
         /// </summary>
         public int CurrentPage { get; set; }
 
         /// <summary>
-        /// The total number of pages to be returned if the ItemsPerPage parameter remains constant.
+        /// The total number of pages that match the filter/search criteria.
+        /// TotalPages = TotalCount / ItemsPerPage.
         /// </summary>
         public int TotalPages { get; set; }
 
         /// <summary>
-        /// The total number of items to be returned.
+        /// The total number of items which match the filter/search criteria.
+        /// If ItemsPerPage is set to 0, Items.Count equals to TotalCount.
+        /// This means, a single page is returned containing all available items which match the filter/search criteria.
         /// </summary>
         public int TotalCount { get; set; }
 
         /// <summary>
-        /// The total number of items if no filtering is applied.
+        /// The total number of items if no filter/search is applied.
         /// </summary>
         public int TotalCountUnfiltered { get; set; }
 
         /// <summary>
-        /// The list of filtered items.
+        /// The paged collection of items which match the filter/search criteria.
+        /// If ItemsPerPage is set >0, ItemsPerPage equals to Items.Count.
         /// </summary>
         public IEnumerable<T> Items { get; set; }
 
+        /// <summary>
+        /// Checks if there are more pages available after the current pagination set.
+        /// </summary>
+        /// <returns><c>true</c> if TotalPages > CurrentPage, otherwise <c>false</c>.</returns>
         public bool HasMorePages()
         {
             return this.TotalPages > this.CurrentPage;
