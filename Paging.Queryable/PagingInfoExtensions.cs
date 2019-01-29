c877a1bf3e2489e59bd52f8897403620e46e374e
Thomas Galliker <thomas@bluewin.ch>
2019-01-29 14:13:30 +0100

    Use ApplyFilter ext method

--- Paging.Queryable/PagingInfoExtensions.cs
+++ Paging.Queryable/PagingInfoExtensions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 
 using System.Linq.Expressions;
@@ -42,76 +41,7 @@ namespace Paging.Queryable
                 // Property-based filter
                 if (pagingInfo.Filter != null)
                 {
-                    foreach (var filter in pagingInfo.Filter)
-                    {
-                        if (filter.Value == null)
-                        {
-                            // Ignore null values
-                            continue;
-                        }
-
-                        if (filter.Value is string stringValue)
-                        {
-                            if (string.IsNullOrEmpty(stringValue))
-                            {
-                                // Ignore null/empty values
-                                continue;
-                            }
-
-                            var op = stringValue[0];
-                            if (op == '>' || op == '<' || op == '=')
-                            {
-                                // If the given string value contains a comparison operator, we apply it
-                                queryable = queryable.TryWhere($"{filter.Key} {filter.Value}");
-                            }
-                            else
-                            {
-                                // No comparison operator means: key.Contains(value)
-                                queryable = queryable.TryWhere($"{filter.Key}.ToString().ToLower().Contains(\"{stringValue.Replace("\"", "").ToLower()}\")");
-                            }
-                        }
-                        else if (filter.Value.IsNumericType() || filter.Value is bool)
-                        {
-                            queryable = queryable.TryWhere($"{filter.Key} == {filter.Value}");
-                        }
-                        else if (filter.Value is DateTime dateTimeValue)
-                        {
-                            queryable = queryable.TryWhere($"{filter.Key} == @0", dateTimeValue);
-                        }
-                        else if (filter.Value is IDictionary<string, object> ranges)
-                        {
-                            foreach (var range in ranges)
-                            {
-                                if (string.IsNullOrEmpty(range.Key))
-                                {
-                                    // Ignore null/empty operator
-                                    continue;
-                                }
-
-                                var op = range.Key[0];
-                                if (op == '>' || op == '<' || op == '=')
-                                {
-                                    if (range.Value is string && DateTime.TryParse($"{range.Value}", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var parsedDateTimeValue))
-                                    {
-                                        queryable = queryable.TryWhere($"{filter.Key} {range.Key} @0", parsedDateTimeValue);
-                                    }
-                                    else
-                                    {
-                                        queryable = queryable.TryWhere($"{filter.Key} {range.Key} @0", range.Value);
-                                    }
-
-                                }
-                                else
-                                {
-                                    throw new NotSupportedException($"Filter range operator '{op}' is currently not supported. Affected property: {filter.Key}.");
-                                }
-                            }
-                        }
-                        else
-                        {
-                            throw new NotSupportedException($"Filter values of type '{filter.Value.GetType().Name}' is currently not supported. Affected property: {filter.Key}.");
-                        }
-                    }
+                    queryable = queryable.ApplyFilter(pagingInfo.Filter);
                 }
 
                 var totalCount = queryable.Count();
@@ -154,39 +84,6 @@ namespace Paging.Queryable
                 var paginationSet = new PaginationSet<TDto>(pagingInfo, dtos, totalCount, totalCountUnfiltered);
                 return paginationSet;
             }
-        }
-
-        private static bool IsNumericType(this object value)
-        {
-            return
-                value is short ||
-                value is ushort ||
-                value is int ||
-                value is uint ||
-                value is long ||
-                value is ulong ||
-                value is float ||
-                value is double ||
-                value is decimal;
-        }
-
-        /// <summary>
-        /// TryWhere is a Where filter which catches <see cref="ParseException"/>.
-        /// We use it in situations where we want to omit wrong filter values, e.g. invalid comparisons like DateTime > new object()
-        /// </summary>
-        private static IQueryable<TSource> TryWhere<TSource>(this IQueryable<TSource> source, string predicate, params object[] args)
-        {
-            try
-            {
-                Trace.WriteLine($"Paging.TryWhere. Predicate: \"{predicate}\". Args.Count: {args.Length}. {(args.Length > 0 ? $"Args: {string.Join(", ", args)}." : "")}");
-                return source.Where(predicate, args);
-            }
-            catch (ParseException parseException)
-            {
-                Trace.WriteLine($"{parseException.Message}. Predicate: \"{predicate}\". Args.Count: {args.Length}. {(args.Length > 0 ? $"Args: {string.Join(", ", args)}." : "")}{Environment.NewLine}{parseException.StackTrace}", "ParseException");
-            }
-
-            return source;
         }
     }
 }
