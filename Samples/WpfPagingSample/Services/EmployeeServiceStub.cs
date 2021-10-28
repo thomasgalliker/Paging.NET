63387eb7ed6b34921a3618b00885465ce4442a20
Thomas Galliker <thomas@bluewin.ch>
2021-10-28 06:30:43 +0200

    Add WPF sample app

---
+++ Samples/WpfPagingSample/Services/EmployeeServiceStub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Paging;
+using WpfPagingSample.Model;
+
+namespace WpfPagingSample.Services
+{
+    public class EmployeeServiceStub : IEmployeeService
+    {
+        private readonly List<Employee> employees;
+
+        public EmployeeServiceStub()
+        {
+            this.employees = Enumerable.Range(1, 1000)
+                .Select(i => new Employee { Id = i, FirstName = $"FirstName {i}", LastName = $"LastName {i}" })
+                .ToList();
+        }
+
+        public Task<PaginationSet<Employee>> GetEmployeesAsync(PagingInfo pagingInfo)
+        {
+            // Use Paging.Queryable NuGet package to perform search/sort/group/paging
+            var skip = (pagingInfo.CurrentPage - 1) * pagingInfo.ItemsPerPage;
+            var take = pagingInfo.ItemsPerPage;
+            var queryable = this.employees.Skip(skip).Take(take);
+
+            var pagingSet = new PaginationSet<Employee>(pagingInfo, queryable, this.employees.Count, this.employees.Count);
+            return Task.FromResult(pagingSet);
+        }
+    }
+}
---
+++ Samples/WpfPagingSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Paging;
+using WpfPagingSample.Model;
+using WpfPagingSample.Services;
+
+namespace WpfPagingSample.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IEmployeeService employeeRepository;
+        private readonly PagingInfo pagingInfo;
+
+        private ObservableCollection<Employee> employees;
+        private int currentPage = 1;
+
+        public MainViewModel(IEmployeeService employeeService)
+        {
+            this.employeeRepository = employeeService;
+
+            this.pagingInfo = new PagingInfo
+            {
+                CurrentPage = 1,
+                ItemsPerPage = 10,
+            };
+
+            this.PreviousPageCommand = new DelegateCommand<object>((o) => this.GoToPreviousPage());
+            this.NextPageCommand = new DelegateCommand<object>((o) => this.GoToNextPage());
+
+            this.DisplayAllEmployees(this.pagingInfo);
+        }
+
+        public ICommand PreviousPageCommand { get; }
+
+        private void GoToPreviousPage()
+        {
+            this.CurrentPage--;
+            this.DisplayAllEmployees(this.pagingInfo);
+        }
+
+        public ICommand NextPageCommand { get; }
+
+        private void GoToNextPage()
+        {
+            this.CurrentPage++;
+            this.DisplayAllEmployees(this.pagingInfo);
+        }
+
+        public int CurrentPage
+        {
+            get => this.currentPage;
+            private set
+            {
+                this.currentPage = value;
+                this.OnPropertyChanged(nameof(this.CurrentPage));
+                this.pagingInfo.CurrentPage = value;
+            }
+        }
+
+        private async void DisplayAllEmployees(PagingInfo pagingInfo)
+        {
+            try
+            {
+                var paginationSet = await this.employeeRepository.GetEmployeesAsync(pagingInfo);
+                this.Employees = new ObservableCollection<Employee>(paginationSet.Items);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load employees: {ex.Message}");
+            }
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => this.employees;
+            private set
+            {
+                if (this.employees != value)
+                {
+                    this.employees = value;
+                    this.OnPropertyChanged(nameof(this.Employees));
+                }
+            }
+        }
+    }
+}
